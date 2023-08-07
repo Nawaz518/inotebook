904fcbdb8b17df4949fbe3bf1701b785395dccbd
Nawaz518 <138968729+Nawaz518@users.noreply.github.com>
2023-08-07 23:36:45 +0530

    db.js

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 const mongoose=require('mongoose');
 const mongoURI="mongodb://localhost:27017/"
 const connectToMongo=()=>{
-    mongoose.connect(mongoURI);
-    console.log("Connected to Mongo Successfully");
+    mongoose.connect(mongoURI).then((data)=>{
+        console.log(`Mongodb connected:${data.connection.host}`)
+    }).catch((err)=>{
+        console.log("unable to connect")
+    })
      }  
 module.exports=connectToMongo;
