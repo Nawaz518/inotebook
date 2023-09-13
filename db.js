98ecb7a826b349c3445706fd4ed668db4e43c517
Nawaz518 <138968729+Nawaz518@users.noreply.github.com>
2023-09-13 23:12:06 +0530

    Updated db.js
    added mongodb atlas uri

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const mongoURI="mongodb://localhost:27017/"
+const mongoURI="mongodb+srv://faizzu438:LmN3pWZEVldriisa@cluster0.clv0gnm.mongodb.net/"
 const connectToMongo=()=>{
     mongoose.connect(mongoURI).then((data)=>{
         console.log(`Mongodb connected:${data.connection.host}`)
